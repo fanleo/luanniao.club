a286cbe126d7ff9cb83db04886dea9e1e3d7bb4a
BenLampson(赵元源) <58603432@qq.com>
2020-04-19 15:38:05 +0800

    first version demo
    first version demo

--- src/Luanniao.Club/Program.cs
+++ src/Luanniao.Club/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using LuanNiao.Blazor.Core;
+using Luanniao.Club.Properties;
 
 namespace Luanniao.Club
 {
@@ -15,7 +16,11 @@ namespace Luanniao.Club
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            Translater.AddLanguageFile(new Translater.SourceItem[] {
+    new Translater.SourceItem(){ CultureName="cn", ItemType= Translater.SourceItemType.OrignalString, Data=Resources.cn },
+    new Translater.SourceItem(){ CultureName="en", ItemType= Translater.SourceItemType.OrignalString, Data=Resources.en },
+            });
+            Translater.ConvertTo("en");
             builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddLuanNiaoBlazor();
             await builder.Build().RunAsync();
