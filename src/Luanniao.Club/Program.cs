d546c24bf3b9edf189d9b9c15504069d0fdc7a11
BenLampson(赵元源) <58603432@qq.com>
2020-04-28 23:15:13 +0800

    add demo
    add demo

--- src/Luanniao.Club/Program.cs
+++ src/Luanniao.Club/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using LuanNiao.Blazor.Component.Antd;
 using LuanNiao.Blazor.Core;
 using Luanniao.Club.Properties;
 
@@ -22,7 +23,7 @@ namespace Luanniao.Club
             });
             Translater.ConvertTo("en");
             //builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddLuanNiaoBlazor();
+            builder.Services.AddLuanNiaoBlazorAntdExtensions();
             await builder.Build().RunAsync();
         }
     }
